f9a2e9b21d7b731c08bb87c210a6fd26ed4a6427
Andres <andresftma@gmail.com>
2025-02-10 11:27:01 -0500

    update login API endpoint to include /api/login path

--- src/paginas/Login.jsx
+++ src/paginas/Login.jsx
@@ -24,7 +24,7 @@ const Login = () => {
     const handleSubmit = async (e) => {
         e.preventDefault()
         try {
-            const url = `https://saludify-backend.onrender.com`
+            const url = `https://saludify-backend.onrender.com/api/login`
             const respuesta = await axios.post(url, form)
             localStorage.setItem("token", respuesta.data.token)
             setAuth(respuesta.data)
